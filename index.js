f8840e2a42baa4bc2969660843dcd6e41db797ce
snehit sharma <snehitsharma@outlook.com>
2023-12-22 08:09:52 -0500

    added email sender function and updated index

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const multer = require('multer');
+const fs = require('fs');
+const XLSX = require('xlsx');
+const { sendEmail } = require('./utils/emailSender');
 
-const app = express();
+const app = express();  
 const port = 3000;
 
 // MongoDB Connection
@@ -10,28 +13,51 @@ mongoose.connect('mongodb://127.0.0.1:27017/mailapp')
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...', err));
 
-// Middleware for parsing JSON and urlencoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Multer for handling file uploads
 const upload = multer({ dest: 'uploads/' });
 
-// Root route
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-// File upload route
 app.post('/upload', upload.single('file'), (req, res) => {
   if (!req.file) {
     return res.status(400).send('No file uploaded.');
   }
-  // Process the file here
-  res.send('File uploaded successfully.');
+
+  let emailAddresses = [];
+  
+  // Process text or Excel file
+  if (req.file.mimetype === 'text/plain') {
+    const fileContent = fs.readFileSync(req.file.path, 'utf8');
+    emailAddresses = fileContent.split('\n').filter(email => email);
+  } else if (req.file.mimetype.includes('excel') || req.file.mimetype.includes('spreadsheetml')) {
+    const workbook = XLSX.readFile(req.file.path);
+    const sheetName = workbook.SheetNames[0];
+    const sheet = workbook.Sheets[sheetName];
+    const data = XLSX.utils.sheet_to_json(sheet);
+    emailAddresses = data.map(row => row.Email);
+  } else {
+    fs.unlinkSync(req.file.path); // Remove the uploaded file
+    return res.status(400).send('Invalid file type.');
+  }
+
+  // Send emails to extracted addresses
+  Promise.all(
+    emailAddresses.map(email => sendEmail(email, 'Your Subject', 'Your Email Content'))
+  )
+  .then(() => {
+    fs.unlinkSync(req.file.path); // Remove the uploaded file
+    res.send('Emails sent successfully.');
+  })
+  .catch(err => {
+    fs.unlinkSync(req.file.path); // Remove the uploaded file
+    res.status(500).send('Error in sending emails.');
+  });
 });
 
-// Start the server
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
--- utils/emailsender.js
+++ utils/emailsender.js
@@ -1,20 +1,34 @@
 // utils/emailSender.js
 
-const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey(process.env.SENDGRID_API_KEY); // Set this in your environment variables
+require('dotenv').config();
+const mailjet = require('node-mailjet')
+    .connect(process.env.MAILJET_API_KEY, process.env.MAILJET_SECRET_KEY);
 
-const sendEmail = (to, subject, text) => {
-    const msg = {
-        to, // recipient
-        from: 'your-email@example.com', // your verified sender email address
-        subject,
-        text,
-    };
+const sendEmail = (toEmail, subject, textContent) => {
+    const request = mailjet
+        .post("send", {'version': 'v3.1'})
+        .request({
+            "Messages":[{
+                "From": {
+                    "Email": "sayitsnehit@gmail.com", // Replace with your Mailjet verified sender email
+                    "Name": "tester" // Replace with your name or sender name
+                },
+                "To": [{
+                    "Email": toEmail,
+                    "Name": "Recipient Name" // Optional, replace or remove if not needed
+                }],
+                "Subject": subject,
+                "TextPart": textContent
+            }]
+        });
 
-    sgMail
-        .send(msg)
-        .then(() => console.log('Email sent'))
-        .catch(error => console.error('Error sending email:', error));
-}
+    return request
+        .then(result => {
+            console.log('Email sent successfully:', result.body);
+        })
+        .catch(err => {
+            console.error('Error sending email:', err.statusCode);
+        });
+};
 
 module.exports = { sendEmail };
